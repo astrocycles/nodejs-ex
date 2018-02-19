e46aea1dcc98deffdfa17a5f0cbed3f549ee41ef
astrocycles <astrocycles@users.noreply.github.com>
2018-02-19 19:47:35 +0100

    server.js changes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express = require('express'),
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('views'));
+
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
